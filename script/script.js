16af89e909d8afa5d324ba14580b96139e223994
Max Kuznetsov <maxkuznec12@gmail.com>
2020-03-30 23:43:55 +0300

    3я усложненка

--- script/script.js
+++ script/script.js
@@ -1,15 +1,44 @@
-let num = 266219;
+let lang = 'ru';
 
-num = num.toString().split('');
+if (lang === 'ru') {
+    console.log(['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']);
+}
+if (lang === 'en') {
+    console.log(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+}
 
-let result = 1;
+switch (lang) {
+    case 'ru':
+        console.log(['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']);
+        break;
+    case 'en':
+        console.log(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
+        break;
+    default:
+        console.log(['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']);
+}
 
-num.forEach(item => {
-    result *= +item;
-});
+let arrLang = [
+    'ru' = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+    'en' = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+];
 
-result = result ** 3;
+let namePerson = 'Артем';
+console.log(namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент');
 
-result = result.toString().slice(0, 2);
+//ДЗ2
+// let num = 266219;
 
-console.log(result); 
+// num = num.toString().split('');
+
+// let result = 1;
+
+// num.forEach(item => {
+//     result *= +item;
+// });
+
+// result = result ** 3;
+
+// result = result.toString().slice(0, 2);
+
+// console.log(result); 
