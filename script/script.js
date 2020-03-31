14f64a7753339ed33b2719f004584d4a426c47a6
Max Kuznetsov <maxkuznec12@gmail.com>
2020-04-01 00:56:05 +0300

    4я усложненка

--- script/script.js
+++ script/script.js
@@ -1,44 +1,9 @@
-let lang = prompt('Выберите язык перевода', 'ru');
-
-if (lang === 'ru') {
-    console.log(['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']);
-}
-if (lang === 'en') {
-    console.log(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
-}
-
-switch (lang) {
-    case 'ru':
-        console.log(['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']);
-        break;
-    case 'en':
-        console.log(['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']);
-        break;
-    default:
-        console.log(['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']);
+function checkArgument(value) {
+    if(typeof value === 'string' && value.trim()) {
+        console.log((value.trim()).length >= 30 ? value.trim().slice(0, 30) + '...' : value.trim());
+    } else {
+        alert('Аргумент должен быть строкой!');
+    }
 }
 
-let arrLang = [];
-arrLang['ru'] = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-arrLang['en'] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-console.log(arrLang[lang]);
-
-let namePerson = prompt('Введите имя:');
-console.log(namePerson === 'Артем' ? 'директор' : namePerson === 'Максим' ? 'преподаватель' : 'студент');
-
-//ДЗ2
-// let num = 266219;
-
-// num = num.toString().split('');
-
-// let result = 1;
-
-// num.forEach(item => {
-//     result *= +item;
-// });
-
-// result = result ** 3;
-
-// result = result.toString().slice(0, 2);
-
-// console.log(result); 
+checkArgument(['    Аргумент должен быть строкой и точка!  ']);
