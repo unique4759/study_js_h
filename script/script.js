62b0b204e8b4fb401c3f561f35d38cc7bb608d1c
Max Kuznetsov <maxkuznec12@gmail.com>
2020-04-02 03:12:31 +0300

    5я усложненка

--- script/script.js
+++ script/script.js
@@ -1,10 +1,14 @@
-function checkArgument(value) {
-    if(typeof value === 'string' && value.trim()) {
-        console.log((value.trim()).length >= 30 ? value.trim().slice(0, 30) + '...' : value.trim());
-    } else {
-        alert('Аргумент должен быть строкой!');
-    }
-}
+let arr = ['368', '643', '278', '1506', '4908', '8978', '423'];
 
-let setValue = prompt('Введите значение:');
-checkArgument(setValue);
+arr.forEach((value) => {
+    if (value.startsWith('2') || value.startsWith('4')) {
+      console.log(value);
+    }
+});
+
+for (let i = 2; i <= 100; i++){
+    for(var j = 2; j <= i; j++){
+        if (i % j == 0) break;
+    }
+    if(j == i) console.log('Делители этого числа: 1 и ' + i);
+}
