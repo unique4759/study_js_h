78b26b9f4db9347727bd2b6f4c59603fcf2af3be
Max Kuznetsov <maxkuznec12@gmail.com>
2020-04-05 16:42:14 +0300

    7я усложненка

--- script/script.js
+++ script/script.js
@@ -1,14 +1,24 @@
-let arr = ['368', '643', '278', '1506', '4908', '8978', '423'];
+let date = new Date(),
+    days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+    findSelect = document.querySelector('.days'),
+    list = '';
 
-arr.forEach((value) => {
-    if (value.startsWith('2') || value.startsWith('4')) {
-      console.log(value);
+days.forEach((day, key) => {
+    if(key == date.getDay()) {
+        if (key == 0 || key == 6) {
+            list += '<li>' + day.bold().italics() + '</li>';
+        } else {
+            list += '<li>' + day.bold() + '</li>';
+        }
+    }
+    else {
+        if (key == 0 || key == 6) {
+            list += '<li>' + day.italics() + '</li>';
+        }
+        else {
+            list += '<li>' + day + '</li>';
+        }
     }
 });
 
-for (let i = 2; i <= 100; i++){
-    for(var j = 2; j <= i; j++){
-        if (i % j == 0) break;
-    }
-    if(j == i) console.log('Делители этого числа: 1 и ' + i);
-}
+findSelect.innerHTML = list;
