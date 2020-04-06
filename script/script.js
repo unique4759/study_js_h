9c8201ae14b74f0f6061addb15f5e6566256d889
Max Kuznetsov <maxkuznec12@gmail.com>
2020-04-06 09:14:14 +0300

    Исправил

--- script/script.js
+++ script/script.js
@@ -1,18 +1,27 @@
 let date = new Date(),
-    days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+    days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
     findSelect = document.querySelector('.days'),
     list = '';
 
 days.forEach((day, key) => {
-    if(key == date.getDay()) {
-        if (key == 0 || key == 6) {
+    let nowDay;
+
+    if(date.getDay() == 0) {
+        nowDay = 6;
+    }
+    else {
+        nowDay = date.getDay() - 1;
+    }
+
+    if(key == nowDay) {
+        if (key == 5 || key == 6) {
             list += '<li>' + day.bold().italics() + '</li>';
         } else {
             list += '<li>' + day.bold() + '</li>';
         }
     }
     else {
-        if (key == 0 || key == 6) {
+        if (key == 5 || key == 6) {
             list += '<li>' + day.italics() + '</li>';
         }
         else {
