020bc6dfec20b086fbe3b00222b151b82f9b908b
Max Kuznetsov <maxkuznec12@gmail.com>
2020-04-12 12:07:50 +0300

    усложненка

--- script/script.js
+++ script/script.js
@@ -1,25 +1,16 @@
-setInterval(function() {
-    let findSelect = document.querySelector('.app'),
-        list = '';
-    let date = new Date(),
-    days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
-    month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'ноября', 'декабря'];
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-    function endingWord(hour) {  
-        return (hour == 1 || (hour > 19 && hour % 10 == 1)) ? 'час' :
-        ((hour > 1 && hour < 5) || (hour > 19 && hour % 10 > 1 && hour % 10 < 5)) ? 'часа' : 'часов'; 
-    } 
+change.addEventListener('click', function () {
+    let color = '';
 
-    function setNum(num){
-        if (num > 0 && num < 10) { 
-            return '0' + num;
-        } else {
-            return num;
-        }
+    for (let i = 0; i < 6; i++) {
+        color += getRandomIntInclusive(0, 9);
     }
 
-    list += '<p>' + 'Сегодня ' + days[date.getDay()] + ', ' + date.getDate() + ' ' + month[date.getMonth()] + ' ' + date.getFullYear() + ' года, ' + date.getHours() + ' ' + endingWord(date.getHours()) + ' ' + date.getMinutes() + ' минут ' + date.getSeconds() + ' секунд' + '</p>';
-    list += '<p>' + setNum(date.getDate()) + '.' + (setNum(date.getMonth() + 1)) + '.' + date.getFullYear() + ' - ' + setNum(date.getHours()) + ':' + setNum(date.getMinutes()) + ':' + setNum(date.getSeconds()) + '</p>';
-
-    findSelect.innerHTML = list;
-}, 1000);
+    document.body.style.backgroundColor = '#' + color;
+    document.querySelector('#color').textContent = '#' + color;
+});
